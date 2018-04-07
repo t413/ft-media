436b930a49d4421428b3df2311d4818dff42226f
Tim O'Brien <timo@t413.com>
2018-04-06 17:33:51 -0700

    Adds CLI11 argument parser

--- ft-media-lib/server.cpp
+++ ft-media-lib/server.cpp
@@ -4,10 +4,17 @@
 namespace ftm {
     using namespace std;
 
-    Server::Server() { }
+    Server::Server() : options_("FT-Media Server") {
+
+        //options_.add_option("-f,--file", filename, "A help string");
+        options_.add_flag("-v,--verbose", verbosity_, "Set the verbosity");
+
+    }
 
     int Server::run(int argc, char *argv[]) {
+        CLI11_PARSE(options_, argc, argv);
         cout << "hello world" << endl;
+        cout << "verbosity is " << verbosity_ << endl;
     }
 
 
--- ft-media-lib/server.h
+++ ft-media-lib/server.h
@@ -1,4 +1,6 @@
+#pragma once
 #include <string>
+#include "addons/CLI11.hpp"
 
 namespace ftm {
 
@@ -6,8 +8,11 @@ namespace ftm {
     public:
         Server();
         int run(int argc, char *argv[]);
-    
 
+
+    protected:
+        CLI::App options_;
+        int verbosity_{1}; //0 none, 1 normal, 2 extra, 3 cray
 
     };
 
